9554260a1a2d807e27a8f6d6f843b186f5cc97d2
pepeworm <66802398+pepeworm@users.noreply.github.com>
2020-08-07 17:59:30 -0600

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,36 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
+const counter = document.querySelector("#counter-number");
+const submitBtn = document.querySelector("button");
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Counter</title>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="./CSS/styles.css">
-</head>
+submitBtn.classList.add("disabled");
 
-<body>
-    <h1>Counter</h1>
+document.querySelector("input[type=text]").addEventListener("input", (e) => {
+    const inputValue = document.querySelector("input[type=text]").value.trim();
 
-    <h3>How the counter works:</h3>
-    <p>Enter any number between <strong>10</strong> and <strong>200</strong> and the counter below will count from 0 to
-        the number you inputted!</p>
+    if (inputValue) {
+        submitBtn.classList.remove("disabled");
+    } else {
+        submitBtn.classList.add("disabled");
+    }
+});
 
-    <div class="container">
-        <div class="counter">
-            <label for="number-input">Enter a number: </label>
-            <input type="text" name="number" id="number-input" placeholder="Number" autocomplete="off" required>
-            <button type="submit">Start Counting!</button>
-            <p id="counter-number" class="push"></p>
-        </div>
-    </div>
-
-    <br>
-    <a href="https://github.com/pepeworm/counter-site" class="code-btn" target="_blank">Source Code</a>
-
-    <script src="./JS/index.js"></script>
-</body>
-
-</html>
+document.querySelector("button").addEventListener("click", () => {
+    counter.classList.add("push");
+    const inputInNum = parseFloat(
+        document.querySelector("input[type=text]").value
+    );
+    if (isNaN(inputInNum)) {
+        alert("That is not a number!");
+        document.querySelector("input[type=text]").value = "";
+    } else if (inputInNum < 10 || inputInNum > 200) {
+        alert(`You can only input numbers between 10 and 200!`);
+        document.querySelector("input[type=text]").value = "";
+    } else {
+        const toInt = Math.floor(inputInNum);
+        let i = 0;
+        let a = setInterval(() => {
+            i++;
+            counter.textContent = i;
+            if (i === toInt) {
+                clearInterval(a);
+                setTimeout(() => {
+                    counter.textContent = ``;
+                    document.querySelector("input[type=text]").value = "";
+                }, 400);
+            }
+        }, 300);
+    }
+});
