39617db333eb37d12c611a5a544aff72a8450d5a
pepeworm <66802398+pepeworm@users.noreply.github.com>
2020-08-07 17:54:36 -0600

    Change alert with message

--- JS/index.js
+++ JS/index.js
@@ -3,6 +3,9 @@ const submitBtn = document.querySelector("button");
 
 submitBtn.classList.add("disabled");
 
+document.querySelector(".error-msg").classList.remove("push");
+document.querySelector(".error-msg").value = "";
+
 document.querySelector("input[type=text]").addEventListener("input", (e) => {
     const inputValue = document.querySelector("input[type=text]").value.trim();
 
@@ -19,11 +22,15 @@ document.querySelector("button").addEventListener("click", () => {
         document.querySelector("input[type=text]").value
     );
     if (isNaN(inputInNum)) {
-        alert("That is not a number!");
-        document.querySelector("input[type=text]").value = "";
+        document.querySelector(".error-msg").value = "You can only input numbers between 10 and 200!";
+        setTimeout(() => {
+            document.querySelector("input[type=text]").value = "";
+        }, 800)
     } else if (inputInNum < 10 || inputInNum > 200) {
-        alert(`You can only input numbers between 10 and 200!`);
-        document.querySelector("input[type=text]").value = "";
+        document.querySelector(".error-msg").value = "You can only input numbers between 10 and 200!";
+        setTimeout(() => {
+            document.querySelector("input[type=text]").value = "";
+        }, 800)
     } else {
         const toInt = Math.floor(inputInNum);
         let i = 0;
